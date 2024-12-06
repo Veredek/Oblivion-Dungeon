84af9e4a8bcbd8c89e378e5644c38d7bca5ce50a
Veredek <hugoribeiro.me@gmail.com>
2024-12-06 14:12:59 -0300

    redimensionalização do jogo corrigida + definições de escala

--- game/main.py
+++ game/main.py
@@ -1,77 +1,103 @@
 import pygame
-import time
-import random
 pygame.init()
 pygame.font.init()
+from src.menu import menu
+from src.new_game import new_game
 
-# ====== mainBox Sizes ======
-mainBox_SIZE = (900, 200)
-mainBox_START = (50,550)
-mainBox_THICKNESS = 8
+# ====== Global Variables ======
+GAME_WIDTH = 1920
+GAME_HEIGHT = 1080
+GAME_TITLE = "DUNGEON"
+GAME_FONT = pygame.font.SysFont("comicsans", 30)
+FPS = 60
+MIN_WINDOW_WIDTH = 854
+MIN_WINDOW_HEIGHT = 480
 
-# ====== GameState ======
-class GameState:
+# ====== Class Screen ======
+class Screen:
     def __init__(self):
-        self.Fullscreen = False
-        self.SCREEN_WIDTH = pygame.display.Info().current_w
-        self.SCREEN_HEIGHT = pygame.display.Info().current_h
-        self.WIN = pygame.display.set_mode((self.SCREEN_WIDTH - 100, self.SCREEN_HEIGHT - 100), pygame.RESIZABLE)
-        pygame.display.set_caption("Game Name")
-        self.FONT = pygame.font.SysFont("comicsans", 30)
-        
+        self.fullscreen = True
+        self.screen_width = pygame.display.Info().current_w
+        self.screen_height = pygame.display.Info().current_h
+        self.fullscreen_scale = min(self.screen_width / GAME_WIDTH, self.screen_height / GAME_HEIGHT)
+        self.window_scale = 0.5
+        self.window_width = self.window_scale * GAME_WIDTH
+        self.window_height = self.window_scale * GAME_HEIGHT
+        self.window = pygame.display.set_mode((self.screen_width, self.screen_height), pygame.FULLSCREEN)
+        self.maximized = False
+        pygame.display.set_caption(GAME_TITLE)
+
     def toggle_fullscreen(self):
-        if self.Fullscreen:
-            self.WIN = pygame.display.set_mode((self.SCREEN_WIDTH - 100, self.SCREEN_HEIGHT - 100))
-            self.Fullscreen = False
+        if self.fullscreen:
+            self.window = pygame.display.set_mode((self.window_width, self.window_height), pygame.RESIZABLE)
+            self.fullscreen = False
         else:
-            self.WIN = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), pygame.FULLSCREEN)
-            self.Fullscreen = True
+            self.window = pygame.display.set_mode((self.screen_width, self.screen_height), pygame.FULLSCREEN)
+            self.fullscreen = True
         pygame.display.flip()
 
-game_state = GameState()
+    def resize(self, event):
+        event_width, event_height = event.size
+        proportion = 1
 
-from src.menu import menu
-#from src.mainBox import draw_mainbox
-        
+        # ------ Maximizando Tela ------
+        if event_width == self.screen_width:
+            self.window_width = event_width
+            self.window_height = event_height
+            self.window_scale = min(self.window_width / GAME_WIDTH, self.window_height / GAME_HEIGHT)
+            self.maximized = True
+
+        # ------ Desmaximizando Tela ------
+        elif self.maximized:
+            self.window_width = event_width
+            self.window_height = event_height
+            self.window_scale = min(self.window_width / GAME_WIDTH, self.window_height / GAME_HEIGHT)
+            self.maximized = False
+
+        # ------ Tela Não Maximizada ------
+        else:
+            if event_width < self.window_width or event_height < self.window_height:
+                proportion = min(event_width / self.window_width, event_height / self.window_height)
+            elif event_width > self.window_width or event_height > self.window_height:
+                proportion = max(event_width / self.window_width, event_height / self.window_height)
+
+            self.window_width = int(proportion * self.window_width)
+            self.window_height = int(proportion * self.window_height)
+
+            if self.window_width < MIN_WINDOW_WIDTH or self.window_height < MIN_WINDOW_HEIGHT:
+                self.window_width, self.window_height = MIN_WINDOW_WIDTH,MIN_WINDOW_HEIGHT
+
+            self.window_scale = self.window_width / GAME_WIDTH
+
+        self.window = pygame.display.set_mode((self.window_width, self.window_height), pygame.RESIZABLE)
+        pygame.display.flip()
+        print(f"New resolution: {self.window_width}x{self.window_height}\n")
+
+# ====== Class GameState ======
+class GameState:
+    def __init__(self, screen): 
+        self.screen = screen
+        self.state = "menu"
+
+# ====== Main Function ======
 def main():
-    #WIN.blit(BG, (0, 0))
+    screen = Screen()
+    game_state = GameState(screen)
     running = True
 
-    #clock = pygame.time.Clock()
-
-    #draw_mainbox(WIN, mainBox_START, mainBox_SIZE, mainBox_THICKNESS)
-
-    choice = menu(game_state)
-
-    if choice != "":
-        pass
-
     while running:
-        #clock.tick(60)
-        for event in pygame.event.get():
-            print(event.type)
-            if event.type == pygame.QUIT:
-                running = False
-                break
 
-            # Posição do mouse e Highlight
-            mouse_pos = pygame.mouse.get_pos()  # Posição atual do mouse
+        if game_state.state == "menu":
+            game_state.state = menu(game_state)
 
-            # Verifica se F11 foi pressionado
-            if event.type == pygame.KEYDOWN:
-                print("KEYDOWN")
-                if event.key == pygame.K_F11:
-                    print("F11")
-                    game_state.toggle_fullscreen()
+        elif game_state.state == "New Game":
+            game_state.state = new_game(game_state)
 
-        #keys = pygame.key.get_pressed()
-        #if keys[pygame.K_UP]:
-
-        pygame.display.flip()
+        elif game_state.state == "Exit Game":
+            running = False
 
     pygame.quit()
 
 if __name__ == "__main__":
     main()
 
-exit()
--- game/src/menu.py
+++ game/src/menu.py
@@ -1,66 +1,96 @@
 import pygame
 
+# ====== Global Variables ======
+GAME_WIDTH = 1920
+GAME_HEIGHT = 1080
+GAME_TITLE = "DUNGEON"
+GAME_NAME_FONT = pygame.font.Font(r"game\src\fonts\OldLondon.ttf",300)
+GAME_FONT = pygame.font.SysFont("comicsans", 30)
+
 def menu(game_state):
-    GameName_FONT = pygame.font.SysFont("Arial", 180)
-    
-    game_state.WIN.fill(0)
-    window_width, window_height = pygame.display.get_window_size()
-    center = window_width // 2
-
-    # Textos e retângulos
-    GameNameText = GameName_FONT.render("GAME NAME", True, "White")
-    NewGameText = game_state.FONT.render("NEW GAME", True, "White")
-    LoadText = game_state.FONT.render("LOAD", True, "White")
-    ExitText = game_state.FONT.render("EXIT", True, "White")
-
-    GameNameText_rect = GameNameText.get_rect(center=(center, 180))
-    NewGameText_rect = NewGameText.get_rect(center=(center, 350))
-    LoadText_rect = LoadText.get_rect(center=(center, 400))
-    ExitText_rect = ExitText.get_rect(center=(center, 450))
-
     running = True
     choice = ""
-
     while running:
-        game_state.WIN.fill(0)  # Redesenha o fundo
-        mouse_pos = pygame.mouse.get_pos()  # Posição atual do mouse
+        game_state.screen.window.fill(0)
 
-        # Renderizar textos com destaque nos botões
-        game_state.WIN.blit(GameNameText, GameNameText_rect)
+        # ------ Definindo Variáveis ------
+        base_surface = pygame.Surface((GAME_WIDTH,GAME_HEIGHT))
+        if game_state.screen.fullscreen:
+            scale = game_state.screen.fullscreen_scale
+            window_width, window_height = game_state.screen.screen_width, game_state.screen.screen_height
+        else:
+            scale = game_state.screen.window_scale
+            window_width, window_height = game_state.screen.window_width, game_state.screen.window_height
+        mouse_pos = pygame.mouse.get_pos()
+        size = (window_width,window_height)
 
-        NewGameText_hover = game_state.FONT.render("NEW GAME", True, "Yellow" if NewGameText_rect.collidepoint(mouse_pos) else "White")
-        LoadText_hover = game_state.FONT.render("LOAD", True, "Yellow" if LoadText_rect.collidepoint(mouse_pos) else "White")
-        ExitText_hover = game_state.FONT.render("EXIT", True, "Yellow" if ExitText_rect.collidepoint(mouse_pos) else "White")
+        # ------ Escalonando ------
+        offset_x = (window_width - GAME_WIDTH * scale) // 2
+        offset_y = (window_height - GAME_HEIGHT * scale) // 2
+        mouse_pos = ((mouse_pos[0] - offset_x) / scale, (mouse_pos[1] - offset_y) / scale)
 
-        game_state.WIN.blit(NewGameText_hover, NewGameText_rect)
-        game_state.WIN.blit(LoadText_hover, LoadText_rect)
-        game_state.WIN.blit(ExitText_hover, ExitText_rect)
+        # ------ Textos e retângulos ------
+        GameNameText = GAME_NAME_FONT.render(GAME_TITLE, True, "White")
+        NewGameText = GAME_FONT.render("NEW GAME", True, "White")
+        LoadText = GAME_FONT.render("LOAD", True, "White")
+        ExitText = GAME_FONT.render("EXIT", True, "White")
 
-        pygame.display.update()
+        GameNameText_rect = GameNameText.get_rect(center=(GAME_WIDTH // 2, GAME_HEIGHT // 5))
+        NewGameText_rect = NewGameText.get_rect(center=(GAME_WIDTH // 2, GAME_HEIGHT // 2.4))
+        LoadText_rect = LoadText.get_rect(center=(GAME_WIDTH // 2, GAME_HEIGHT // 2.4 + 60))
+        ExitText_rect = ExitText.get_rect(center=(GAME_WIDTH // 2, GAME_HEIGHT // 2.4 + 120))
 
+        # ------ Highlight Buttons ------
+        NewGameText_hover = GAME_FONT.render("NEW GAME", True, "Yellow" if NewGameText_rect.collidepoint(mouse_pos) else "White")
+        LoadText_hover = GAME_FONT.render("LOAD", True, "Yellow" if LoadText_rect.collidepoint(mouse_pos) else "White")
+        ExitText_hover = GAME_FONT.render("EXIT", True, "Yellow" if ExitText_rect.collidepoint(mouse_pos) else "White")
+
+        # ------ Base Surface Blit ------
+        base_surface.blit(GameNameText, GameNameText_rect)
+        base_surface.blit(NewGameText_hover, NewGameText_rect)
+        base_surface.blit(LoadText_hover, LoadText_rect)
+        base_surface.blit(ExitText_hover, ExitText_rect)
+
+        # ------ Window Blit ------
+        scaled_surface = pygame.transform.scale(base_surface, (int(GAME_WIDTH * scale), int(GAME_HEIGHT * scale)))
+        game_state.screen.window.blit(scaled_surface, (offset_x, offset_y))
+        pygame.display.flip()
+
+        # ------ Detectando Eventos ------
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            # Detecta eventos de redimensionamento
+            elif event.type == pygame.VIDEORESIZE:
+                if event.size != size:
+                    print(f"Event Size:{event.size},\nWindow Size:{size}\n")
+                    if game_state.screen.fullscreen == False:
+                        game_state.screen.resize(event)
+                        break
+                    size = event.size
+
+            # Detecta Click Esquerdo do Mouse
+            elif event.type == pygame.MOUSEBUTTONDOWN:
                 if NewGameText_rect.collidepoint(mouse_pos):
                     choice = "New Game"
+                    print(f"{choice}\n")
                     running = False
                 elif LoadText_rect.collidepoint(mouse_pos):
                     choice = "Load Game"
-                    print("Load Game")
+                    print(f"{choice}\n")
                     running = False
                 elif ExitText_rect.collidepoint(mouse_pos):
                     choice = "Exit Game"
-                    pygame.quit()
-                    exit()
+                    print(f"{choice}\n")
+                    running = False
             
             # Verifica se F11 foi pressionado
-            if event.type == pygame.KEYDOWN:
-                print("KEYDOWN")
+            elif event.type == pygame.KEYDOWN:
+                print("\nKEYDOWN")
                 if event.key == pygame.K_F11:
-                    print("F11")
-                    game_state.toggle_fullscreen()
+                    print("\nF11")
+                    game_state.screen.toggle_fullscreen()
 
     return choice
